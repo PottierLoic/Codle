f215f0d0b03bff92472083fd408f7282fabc853e
Loïc Pottier <loic.pottier@live.fr>
2025-03-22 13:01:29 +0100

    fix: update todo.tsx according to previous refactor

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
-import Header from "@/components/Header"
-import Footer from "@/components/Footer"
+import GameLayout from "@/components/common/layout/GameLayout";
 
 export default function TodoGame() {
-   return (
-       <div className="min-h-screen bg-gray-900 text-white flex flex-col">
-            <Header />
-            <main className="flex-1 flex flex-col items-center p-4">
-              <h1 className="text-4xl font-bold mb-4">Not done yet</h1>
-            </main>
-            <Footer />
-          </div>
+    return (
+        <GameLayout>
+            <h1 className="text-4xl font-bold mb-4">Not done yet</h1>
+        </GameLayout>
     )
 }
